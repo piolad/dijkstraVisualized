f89c55ef26be1f63140a9e3fa0afc459a284ee15
DESKTOP-JC7S42N\Piotr <piolad2003@gmail.com>
2023-04-09 17:09:06 +0200

    add the mechanism behind the selection of A and B

--- script.js
+++ script.js
@@ -7,19 +7,51 @@ window.onload =()=>{
 const border_width = "1.75px"
 const gridElemWidth = "15px"
 
+let pntA = null
+let pntB = null
+
+
+
+function gridClick(event){
+    trg = event.target;
+    console.log(trg);
+    if(trg.classList.contains("pntA")){
+        pntA.classList.remove("pntA")
+        pntA = null
+        trg.classList.remove("pntA")
+    }
+    else if(trg.classList.contains("pntB")){
+        pntB.classList.remove("pntB")
+        pntB = null
+        trg.classList.remove("pntB")
+    }
+    else{
+        if(pntA != null){
+            if(pntB!=null)
+                pntB.classList.remove("pntB")
+            trg.classList.add("pntB")
+            pntB = trg
+        }else if(pntA == null){
+            trg.classList.add("pntA")
+            pntA = trg
+        }
+    }   
+}
+
 
 function init_grid(n){
-    grird = document.getElementById('grid');
-    console.log(grird);
+    grid = document.getElementById('grid');
+    console.log(grid);
 
-    grird.innerHTML = ''
+    grid.innerHTML = ''
     for (let i = 0; i < n*n; i++) {
-        grird.innerHTML += "<div><div/>";
+        gridElement = document.createElement("div");
+        gridElement.addEventListener("click",gridClick, false);
+
+        grid.appendChild(gridElement);
+        // grid.innerHTML += "<div><div/>";
         
     }
 
     // grird.style.grid-template-rows = "repeat(" +n +", " + gridElemWidth+ ");";
-
-
-
 }
