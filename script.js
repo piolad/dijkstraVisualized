6fdcf56fcdd9df30a4fec28eb26f87b11c1b568a
DESKTOP-JC7S42N\Piotr <piolad2003@gmail.com>
2023-05-31 17:22:19 +0200

    backbone for dijkstra

--- script.js
+++ script.js
@@ -14,18 +14,116 @@ let pntA = null
 let pntB = null
 
 
-function solve(){
+function getSurroundings(pnt){
+    result = []
+    
+    ns = pnt.nextSibling
+    ps = pnt.previousSibling
+    if(ns!=null){
+        result.push(ns)
+        for(i=0; i<grid_n-1; i++){
+            if(ns!=null){
+                ns = ns.nextSibling
+            }
+        }
+        if(ns!=null){
+            result.push(ns)
+        }
+    }
+    if(ps!=null){
+        result.push(ps)
+        for(i=0; i<grid_n-1; i++){
+            if(ps!=null){
+                ps = ps.previousSibling
+            }
+        }
+        if(ps!=null){
+            result.push(ps)
+        }
+    }
+
+    return result;
+}
+
+const distances = {};
+
+function dijkstraSolve(){
+
+    console.log("b");
     if(pntA == null || pntB == null){
         return null;
     }
+    // const distances = {};
+    const previous = {}
+    const unvisited = new Set();
     
+    for(let row of grid){
+        for(let el of row){
+            distances[el] = Infinity;
+            previous[el] = null;
+            unvisited.add(el);
+        }
+    }
+    distances[pntA] = 0;
+
+    console.log(distances)
+
+    while(unvisited.size > 0){
+        let currentEl = null;
+        let minDist = Infinity;
     
+        for(const el of unvisited){
+            if(distances[el] < minDist){
+                currentEl = el;
+                minDist = distances[el];
+            }
+        }
+
+        if(currentEl === pntB){
+            // console.log(distances)
+            // console.log(previous)
+            // console.log(unvisited)
+
+
+            const path = [];
+            while(previous[currentEl]){
+                path.push(currentEl)
+                currentEl = previous[currentEl];
+            }
+            path.push(pntA)
+            path.reverse()
+            console.log(path)
+            return path
+        }
+
+        unvisited.delete(currentEl);
+
+        if(currentEl !== null && distances[currentEl] !== Infinity){
+            const surround = getSurroundings(currentEl);
+
+            for(const neighbor of surround){
+                const dst = distances[currentEl] + 1;
+
+                if(dst < distances[neighbor]){
+                    distances[neighbor] = dst;
+                    previous[neighbor] = currentEl;
+                }
+            }
+
+        }
+    }
+
+    
+    return [];
 }
 
 
 function gridClick(event){
+
     trg = event.target;
     console.log(trg);
+    // console.log(getSurroundings(trg));
+
     if(trg.classList.contains("pntA")){
         pntA.classList.remove("pntA")
         pntA = null
@@ -76,14 +174,8 @@ function init_grid(n){
         gridElement.addEventListener("click",gridClick, false);
 
         gridW.appendChild(gridElement);
-
-        // console.log(`[${(i-i%n)/n}][${i%n}]`);
         grid[(i-i%n)/n][i%n] = gridElement;
-        
-        // grid.innerHTML += "<div><div/>";
+    
         
     }
-    // console.table(grid)
-
-    // grird.style.grid-template-rows = "repeat(" +n +", " + gridElemWidth+ ");";
 }
